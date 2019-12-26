ddbc464d5e09ce9c7224bc73a643ad9703430a99
destiny_abellana07 <destiny_abellana07@outlook.com>
2019-12-26 11:32:55 +0800

    Moved HttpRSSClient from Utils to Services

--- RSSFetcherService/Services/HttpRSSClient.cs
+++ RSSFetcherService/Services/HttpRSSClient.cs
@@ -2,7 +2,7 @@
 using System.Net;
 using System.Threading.Tasks;
 
-namespace RSSFetcherService.Utils
+namespace RSSFetcherService.Services
 {
     public class HttpRSSClient : IHttpRSSClient
     {
--- RSSFetcherService/Services/IHttpRSSClient.cs
+++ RSSFetcherService/Services/IHttpRSSClient.cs
@@ -1,6 +1,6 @@
 ﻿using System.Threading.Tasks;
 
-namespace RSSFetcherService.Utils
+namespace RSSFetcherService.Services
 {
     public interface IHttpRSSClient
     {
