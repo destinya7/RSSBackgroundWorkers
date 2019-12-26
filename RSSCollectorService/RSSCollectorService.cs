f993fc8b9e2fbba782c58741dffa10a1684523d9
destiny_abellana07 <destiny_abellana07@outlook.com>
2019-12-26 13:41:25 +0800

    Implemented timer and core

--- RSSCollectorService/RSSCollectorService.cs
+++ RSSCollectorService/RSSCollectorService.cs
@@ -1,23 +1,48 @@
 ﻿using System.ServiceProcess;
-using RSSBackgroundWorkerBusiness.Repositories;
-using RSSBackgroundWorkerBusiness.DAL;
+using System.Timers;
+using RSSCollectorService.Core;
+using RSSCollectorService.Services;
 
 namespace RSSCollectorService
 {
     public partial class RSSCollectorService : ServiceBase
     {
-        public RSSCollectorService()
+        private Timer _timer;
+
+        private ICollectorCore _collectorCore;
+        private ILoggerService _logger;
+
+        public RSSCollectorService(
+            ICollectorCore collectorCore,
+            ILoggerService logger
+        )
         {
             InitializeComponent();
+
+            _collectorCore = collectorCore;
+            _logger = logger;
+            _timer = new Timer();
         }
 
         protected override void OnStart(string[] args)
         {
-            
+            _logger.Debug("Service Started");
+
+            _timer.Elapsed += new ElapsedEventHandler(Timer_Elapsed);
+            _timer.Interval = 216000000;
+            _timer.Enabled = true;
+            _timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _collectorCore.CollectUrls();
         }
 
         protected override void OnStop()
         {
+            _logger.Debug("Service Stopped");
+            _timer.Stop();
         }
     }
 }
