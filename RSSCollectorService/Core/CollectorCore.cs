833a1c79ec26afbfe269f6247c4b7187c6f1dfb2
destiny_abellana07 <destiny_abellana07@outlook.com>
2019-12-26 14:28:27 +0800

    Added more logging

--- RSSCollectorService/Core/CollectorCore.cs
+++ RSSCollectorService/Core/CollectorCore.cs
@@ -1,5 +1,6 @@
 ﻿using RSSBackgroundWorkerBusiness.Repositories;
 using RSSCollectorService.Services;
+using System;
 
 namespace RSSCollectorService.Core
 {
@@ -7,24 +8,41 @@ namespace RSSCollectorService.Core
     {
         private IChannelRepository _channelRepository;
         private IWorkerQueuePublisher _workerQueuePublisher;
+        private ILoggerService _logger;
 
         public CollectorCore(
             IChannelRepository channelRepository,
-            IWorkerQueuePublisher workerQueuePublisher
+            IWorkerQueuePublisher workerQueuePublisher,
+            ILoggerService logger
         )
         {
             _channelRepository = channelRepository;
             _workerQueuePublisher = workerQueuePublisher;
+            _logger = logger;
         }
 
         public async void CollectUrls()
         {
-            var channels =
-                await _channelRepository.GetChannelsLastUpdatedWithin(60);
-
-            foreach(var channel in channels)
+            try
             {
-                _workerQueuePublisher.PublishMessage(channel.RSS_URL);
+                _logger.Debug("Fetching Channels in repo");
+
+                var channels =
+                    await _channelRepository.GetChannelsLastUpdatedWithin(60);
+
+                _logger.Debug("Done fetching Channels in repo");
+
+                foreach (var channel in channels)
+                {
+                    _logger.Debug($"Publishing {channel.RSS_URL}");
+
+                    _workerQueuePublisher.PublishMessage(channel.RSS_URL);
+
+                    _logger.Debug($"Published {channel.RSS_URL}");
+                }
+            } catch (Exception e)
+            {
+                _logger.Debug($"Error Collecting Urls {e.ToString()}");
             }
         }
     }
